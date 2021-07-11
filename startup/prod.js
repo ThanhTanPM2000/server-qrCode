73d29b36845285073268c3976f4b9ab314b4af07
Nguyen Thanh Tan <thanhtanpm2000@gmail.com>
2021-07-11 22:23:12 +0700

    add multi origin

--- startup/prod.js
+++ startup/prod.js
@@ -2,14 +2,22 @@ const helmet = require("helmet");
 const cors = require("cors");
 const compression = require("compression");
 
+var whitelist = [
+  "http://localhost:3000",
+  "https://vlu-qrcode-client.herokuapp.com",
+];
+
 module.exports = function (app) {
   app.use(helmet());
   app.use(
     cors({
-      origin: [
-        "https://vlu-qrcode-client.herokuapp.com/",
-        "http://localhost:3000/",
-      ],
+      origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
     })
   );
   app.use(compression());
